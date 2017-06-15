bbe8967d60c14802704299ed8e513890b007f9d4
icthis3t7 <icthis3t7@gmail.com>
2017-06-14 21:41:39 -0500

    base app

---
+++ src/app/app.js
@@ -0,0 +1,16 @@
+var materials = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+
+];
+
+[8,6,7,9]
+
+let materialsLength2 = materials.map((material) => {
+  return material.length;
+
+});
+
+console.log(materialsLength2)
